f0aa851db9da36aec0548c378c967a7dc4a4e134
Dexter Ng <DNg@lcs.on.ca>
2024-01-10 11:34:45 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Dexter Ng on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
